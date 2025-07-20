9cd81483db1084853feb8f14e9ab3990d34b7013
viitorags <vitorgabriels1000@gmail.com>
2025-07-19 21:56:18 -0300

    doc: update doc

--- docs/docs.go
+++ docs/docs.go
@@ -389,7 +389,7 @@ const docTemplate = `{
 // SwaggerInfo holds exported Swagger Info so clients can modify it
 var SwaggerInfo = &swag.Spec{
 	Version:          "1.0",
-	Host:             "localhost:8080",
+	Host:             "squid-app-ti4wd.ondigitalocean.app",
 	BasePath:         "/api/v1",
 	Schemes:          []string{},
 	Title:            "GoWork API",
